9efba72d61a74cb15b336f422b108d46c9e11478
Dhruv-AFK <dhruvjha1239@gmail.com>
2026-04-07 19:50:45 +0530

    updated

--- counter.js
+++ counter.js
@@ -1,10 +1,9 @@
 //document.getElementById("count-el").innerText = 5
         
 
-//let count = 0
-//console.log(count) 
+let count = 0
+console.log(count) 
 
-// let myAge = 35
-// let humanDogRatio = 7
-// let myDogAge = myAge * humanDogRatio
-// console.log(myDogAge)
+let myAge = 35let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
